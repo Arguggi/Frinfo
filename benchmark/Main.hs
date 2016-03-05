a9a2d2e19900083675de381449eb5f4db456ede9
Arguggi <git@arguggi.co.uk>
2016-03-05 13:48:55 +0100

    Add MVar to benchmark

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -11,9 +11,10 @@ import qualified Control.Monad.State.Strict as S
 
 main :: IO ()
 main = do
-    mvar <- Conc.newMVar "Test Song"
+    songMVar <- Conc.newMVar "Test Song"
+    emailMVar <- Conc.newMVar 0
     let sState = FF.systemState FF.defaultMyState
-        fresStrucMvar = FF.defaultMyState { FF.systemState = sState { FF.dbusState = mvar }}
+        fresStrucMvar = FF.defaultMyState { FF.systemState = sState { FF.dbusState = songMVar, FF.emailState = emailMVar }}
     defaultMain [
         bgroup "frinfo" [bench "print Dzen" $ nfIO (S.evalStateT (FF.printDzen F.freeStruc) fresStrucMvar)]
         ]
