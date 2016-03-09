0d3891122e772f82870b7ec23bcb2165ddb7663a
Arguggi <git@arguggi.co.uk>
2016-03-09 12:16:34 +0100

    Check if MPD is actually running
    If MPD isn't running best to wait some time before trying to IDLE for the status
    again.

--- src/Frinfo/MPD.hs
+++ src/Frinfo/MPD.hs
@@ -3,30 +3,46 @@
 module Frinfo.MPD where
 
 import qualified Control.Concurrent    as Conc
-import           Control.Monad
 import           Data.Map              as Map
 import qualified Data.Text             as T
 import           Formatting            (sformat, (%))
 import qualified Formatting.Formatters as Format
 import qualified Network.MPD           as MPD
 
--- | Updates the T.Text in the Conc.MVar when MPD changes song
+-- | Setup the 'Conc.MVar' by first checking if MPD is running and then forks by calling
+-- 'startMPDThread'
 connectToMPD :: Conc.MVar T.Text -> IO Conc.ThreadId
 connectToMPD mvar = do
     -- Get the playing song when we first start
     songResponse <- MPD.withMPD MPD.currentSong
-    let songInfo = getSongInfo songResponse
-    updateSong mvar songInfo
+    case songResponse of
+        -- If we are unable to get the song name
+        -- we don't have to update the 'Conc.MVar'
+        (Left _ ) -> startMPDThread mvar Nothing
+        (Right song) -> do
+            let songInfo = getSongInfo song
+            startMPDThread mvar songInfo
+
+-- | Updates the 'Conc.Mvar' if necessary and then forks a new thread that
+-- keeps updating it
+startMPDThread :: Conc.MVar T.Text -> Maybe T.Text -> IO Conc.ThreadId
+startMPDThread mvar text = do
+    updateSong mvar text
     Conc.forkIO (loop mvar)
 
 -- | IDLE for a 'MPD.PlayerS' update and then update the 'Conc.MVar'
 loop :: Conc.MVar T.Text -> IO ()
-loop mvar = forever $ do
+loop mvar = do
     songResponse <- MPD.withMPD $ do
         _ <- MPD.idle [MPD.PlayerS]
         MPD.currentSong
-    let songInfo = getSongInfo songResponse
-    updateSong mvar songInfo
+    case songResponse of
+        -- If we can't connect to MPD wait for some time before trying again
+        (Left _) -> Conc.threadDelay 1000000 >> loop mvar
+        (Right response) -> do
+            let songInfo = getSongInfo response
+            updateSong mvar songInfo
+            loop mvar
 
 -- | If the 'T.Text' is 'Nothing' we don't have to update the 'Conc.Mvar'
 updateSong :: Conc.MVar T.Text -> Maybe T.Text -> IO ()
@@ -34,9 +50,8 @@ updateSong _ Nothing = return ()
 updateSong mvar (Just text) = Conc.modifyMVar_ mvar $ \_ -> return text
 
 -- | Get the song info from the MPD response
-getSongInfo :: MPD.Response (Maybe MPD.Song) -> Maybe T.Text
-getSongInfo (Left _) = Nothing
-getSongInfo (Right info) = do
+getSongInfo :: Maybe MPD.Song -> Maybe T.Text
+getSongInfo info = do
     song <- info
     let tags = MPD.sgTags song
     -- 'MPD.sgTags' is of type 'Map.Map' 'MPD.Metadata' ['MPD.Value']
